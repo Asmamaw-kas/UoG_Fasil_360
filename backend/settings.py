bace50a088f496eb68407b176846b263744f0a21
Asmamaw-kas <asmamawkassie478@gmail.com>
2025-10-24 22:33:49 +0300

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -120,13 +120,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', ''),
+        'USER': os.environ.get('DB_USER', ''),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', ''),
+        'PORT': os.environ.get('DB_PORT', '5432'),
+    }
 }
 
 MEDIA_URL = '/media/'
